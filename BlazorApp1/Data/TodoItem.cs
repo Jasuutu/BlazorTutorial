15e3fd2af7b6ab75479bcb1bee3ac19e467e2d32
Johnathan Schultz <jschultz@bstglobal.com>
2019-12-05 07:40:17 -0500

    Initial local commit

---
+++ BlazorApp1/Data/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp1.Data
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+        public bool IsCleared { get; set; }
+    }
+}
---
+++ BlazorApp1/Data/TodoService.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BlazorApp1.Data
+{
+    public class TodoService
+    {
+        private List<TodoItem> todos;
+        private int currentId;
+
+        public TodoService()
+        {
+            todos = new List<TodoItem> { new TodoItem {Id = 1, Title = "Test" } };
+            currentId = 1;
+        }
+
+        public IEnumerable<TodoItem> Todos => todos;
+
+        public void AddTodoItem(string todoTitle)
+        {
+            var todo = new TodoItem{Id = ++currentId, Title = todoTitle};
+            todos.Add(todo);
+        }
+
+        public void ClearDoneTodos()
+        {
+            if (todos.Any())
+            {
+                todos.ForEach(todo =>
+                {
+                    if (todo.IsDone)
+                    {
+                        todo.IsCleared = true;
+                    }
+                });
+            }
+        }
+
+        public void ResetAllTodos()
+        {
+            if (todos.Any())
+            {
+                todos.ForEach(todo => todo.IsCleared = false);
+            }
+        }
+    }
+}
